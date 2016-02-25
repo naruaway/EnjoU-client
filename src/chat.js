fa38794e93002a8936f83c98864a1c0cb67e1c43
Narutaka Awaya <naru@pixmag.net>
2016-02-25 09:41:08 +0900

    Implement replyTo

--- src/chat.js
+++ src/chat.js
@@ -5,33 +5,65 @@ import V from './view'
 import _ from 'lodash'
 import {segment} from './lib/tiny-segmenter'
 
+
+function startsWith(target) {
+  return this.merge(most.of(target))
+}
+
 function intent({WS, DOM, ROUTER, id}) {
   return {
+    /*
+    clickMessage$: DOM.select('.message').events('click')
+      .map(ev => parseInt(ev.currentTarget.dataset.id))
+      .tap(messageId => { document.querySelector('#post-text').value += ` @${messageId} ` }),
+      */
+
     postText$: DOM.select('#post').events('submit').tap(ev => ev.preventDefault())
-    .map(ev => {
-      const elm = ev.currentTarget.querySelector('#post-text')
-      const value = elm.value
-      elm.value = ''
-      return value
-    }).multicast(),
+      .map(ev => {
+        const elm = ev.currentTarget.querySelector('#post-text')
+        const value = elm.value
+        elm.value = ''
+        return value
+      }).multicast(),
+
+    changeText$: DOM.select('#post-text').events('input').map(ev => ev.currentTarget.value),
+
     initialMessages$: WS.get('initial messages'),
+
     newMessage$: WS.get('new message'),
   }
 }
 
 function model(actions) {
+  const selectedMessages$ = actions.changeText$
+    .merge(actions.postText$.constant(''))
+    .map(text => {
+      const m = text.match(/@([1-9][0-9]+|[1-9])/g)
+      return new Set((m ? m : []).map(i => parseInt(i.slice(1))))
+    }).debounce(500)::startsWith(new Set())
+
   const messages$ = most.merge(actions.initialMessages$, actions.newMessage$).scan((a, c) => {
     if (a === null) return c
-    return (a.unshift(c), a)
+    return [c, ...a]
   }, null).skip(1)
-  return messages$.map(messages => ({ messages }))
+
+  return most.combineArray((messages, selectedMessages) => (
+    {messages, selectedMessages}
+  ), [messages$, selectedMessages$])
 }
 
-function view({messages}, id) {
+function view({messages, selectedMessages}, id) {
   function createMessageElm(message) {
-    return h('li', {key: message.messageId, style: {
-      color: `rgba(0, ${(message.score + 5) * 50}, 0, 1)`,
-    }}, message.contents)
+    return h('li.message', {
+      key: message.messageId,
+      style: {
+        color: `rgba(0, ${(message.score + 5) * 10}, 0, ${selectedMessages.has(message.messageId) ? 1 : 0.4})`,
+      },
+      attrs: {
+        'data-id': message.messageId,
+      },
+    }
+    , [h('span', `${message.messageId} `), h('span.message-contents', message.contents)])
   }
 
   return h('div', [
